3132298d455973116a8014998a96c0e740ab7b20
zhongyu <zhongyu@itcast.cn>
2025-12-13 16:03:42 +0800

    fix: 添加publicPath配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './' // 添加这行配置，使用相对路径引用资源
 })
